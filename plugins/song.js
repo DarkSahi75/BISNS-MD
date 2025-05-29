ac38ef9e347f545188ff7a7f154f56e5f0e92ee2
DINU-FXMD <darkdenuwan5@gmail.com>
2025-05-29 22:35:13 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -7,7 +7,42 @@ const config = require("../settings");
 // Get prefix dynamically from settings or fallback
 const prefix = config.PREFIX || ".";
 
+const { ytsearch } = require('@dark-yasiya/scrap'); // <-- මෙය එකතු කරන්න
 
+cmd({ 
+    pattern: "devilnewvv", 
+    react: "🗞️", 
+    desc: "Download YouTube video", 
+    category: "download", 
+    use: '.video <YouTube URL or Name>', 
+    filename: __filename 
+}, async (conn, mek, m, { from, q, reply }) => { 
+    try { 
+        if (!q) return await reply("\`⚠️ Give Link Or Text\`");
+
+        const yt = await ytsearch(q);
+        if (yt.videos.length < 1) return reply("\`❌ Not Result || හමු නොවීය\`");
+
+        let yts = yt.videos[0];  
+        let apiUrl = `https://apis.davidcyriltech.my.id/download/ytmp4?url=${encodeURIComponent(yts.url)}`;
+
+        let response = await fetch(apiUrl);
+        let data = await response.json();
+
+        if (!data || data.status !== 200 || !data.result || !data.result.download_url) {
+            return reply("\`⚠️ Download Link Not || ගැටලුවක්...😴\`");
+        }
+
+        await conn.sendMessage(from, { 
+            video: { url: data.result.download_url }, 
+            mimetype: "video/mp4" 
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.error(e);
+        reply("\`❌ Download ERROR- TRY ANOTHER TIME|| යම් කිසි ගැටලුවක්🤧.\`");
+    }
+});
 
 cmd({ 
     pattern: "devilnewv", 
