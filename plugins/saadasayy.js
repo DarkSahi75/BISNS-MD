a49550366e467ffa30728902b5a14863a2ae675c
DINU-FXMD <darkdenuwan5@gmail.com>
2025-06-04 16:01:10 +0530

    Update saadasayy.js

--- plugins/saadasayy.js
+++ plugins/saadasayy.js
@@ -131,3 +131,33 @@ async (conn, mek, m, { from, q, reply }) => {
     return reply(`❌ Error\n\n${e.message}`);
   }
 });       
+
+
+cmd({
+  pattern: "tikaud",
+  alias: ["tt", "ttdl", "tiktokdl"],
+  react: '🎩',
+  desc: "Download TikTok video (WM) + Audio",
+  category: "download",
+  use: '.tiktok <tiktok url>',
+  filename: __filename
+},
+async (conn, mek, m, { from, q, reply }) => {
+  try {
+    if (!q) return await reply('📌 TikTok වීඩියෝ link එකක් දෙන්න.');
+    if (!q.includes("tiktok")) return await reply("🔗 වලංගු TikTok URL එකක් දෙන්න.");
+
+    const res = await fetch(`https://darksadasyt-tiktokdl.vercel.app/api/tiktok?q=${q}`);
+    const data = await res.json();
+
+    // Send watermark video
+    await conn.sendMessage(from, { video: { url: data.watermark }, caption: "🎥" }, { quoted: mek });
+
+    // Send audio as voice message (PTT)
+    await conn.sendMessage(from, { audio: { url: data.music }, mimetype: 'audio/mp4', ptt: true }, { quoted: mek });
+
+  } catch (e) {
+    console.log(e);
+    return reply(`❌ Error\n\n${e.message}`);
+  }
+});
