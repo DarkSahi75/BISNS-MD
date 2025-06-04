b82e6cc9b4bde16b47c424168a8dca09b480b221
DINU-FXMD <darkdenuwan5@gmail.com>
2025-06-04 13:10:34 +0530

    Create settings.js

---
+++ plugins/settings.js
@@ -0,0 +1,41 @@
+const config = require('../settings')
+const {
+    cmd,
+    commands
+} = require('../lib/command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson, jsonformat} = require('../lib/functions')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const fg = require('api-dylux');
+const si = require('systeminformation')
+const os = require('os')
+var { get_set , input_set } = require('../lib/set_db') 
+const  bot = `94765527900`
+
+
+
+var BOTOW = ''
+if(config.LANG === 'SI') BOTOW = "*ඔබ Bot\'s හිමිකරු හෝ  උපපරිපාලක නොවේ !*"
+else BOTOW = "*You are not bot\'s owner or moderator !*"
+
+
+
+cmd({
+    pattern: "setp",
+    react: "🗣️",
+    desc: "To Activate auto news",
+    category: "main",
+    use: '.setprefix .',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!isOwner) return await reply(BOTOW)
+    if ( config.PREFIX == q) return reply(`Succesfully Song Change To This Section`)
+  await input_set('PREFIX' , q)
+  return reply(`PREFIX was changed`)
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
