6dc7a50ddb97ae9fa53cdb1ef47e55a433cd41dc
DINU-FXMD <darkdenuwan5@gmail.com>
2025-06-22 19:22:35 +0530

    Update Vv.js

--- plugins/Vv.js
+++ plugins/Vv.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
-const config = require('../config');
-const { cmd, commands } = require('../command');
+const config = require('../settings');
+const { cmd, commands } = require('../lib/command');
 
 const fs = require("fs");
 
