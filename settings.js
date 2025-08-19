1ac8e2f904652df81f24b3eb2610eca837ff44d2
DINU-FXMD <darkdenuwan5@gmail.com>
2025-08-19 22:48:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'DINUWH-MD=NFABXLhD#ENAuZzwQe7HSBeGklQUpFZoyfSPlh4JcEZl8Ok_y-ng' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'DINUWH-MD=Ws4ExJxK#KXec52v7pkQE_A8Lh9ERRB6zhn_WNlXOyG6fqeVIVDk' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94720244981,94764582504,94711036044' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '/' ,
