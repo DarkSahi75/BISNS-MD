605b222efd39c1361f4c978f9aff2f2b56b5b934
DINU-FXMD <darkdenuwan5@gmail.com>
2025-09-03 10:33:52 +0530

    Update Auto react.js

--- plugins/Auto react.js
+++ plugins/Auto react.js
@@ -1,4 +1,39 @@
 const { cmd } = require('../lib/command');
+
+ const { cmd } = require('../lib/command');
+
+// Auto Follow & React to CYBER VENOM newsle
+
+
+ 
+
+// Auto Follow & React to CYBER VENOM newsletter only
+cmd({
+    on: "body"
+}, async (conn, mek, m, { }) => {
+    try {
+        // CYBER VENOM ONLY
+        const newsletterId = " 120363420902671322@newsletter";
+        const metadata = await conn.newsletterMetadata("jid", newsletterId);
+
+        // Check if not following and follow
+        if (metadata.viewer_metadata === null) {
+            await conn.newsletterFollow(newsletterId);
+            console.log("CYBER CHANNEL FOLLOW ✅");
+        }
+
+        // React to messages
+        if (mek?.key?.server_id) {
+            const id = mek.key.server_id;
+            await conn.newsletterReactMessage(newsletterId, id, "💙"); // React with a yellow heart emoji
+        }
+
+    } catch (e) {
+        console.log("CYBER VENOM AUTO FOLLOW ERROR:", e.message);
+    }
+});
+ 
+
 module.exports = async (robin, mek, store) => {
   try {
     let body =
